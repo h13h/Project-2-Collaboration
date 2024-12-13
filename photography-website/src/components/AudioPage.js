a878fab816474846fdd410cc3ffcb16afb070467
h13h <harshal.patel1305@gmail.com>
2024-12-13 02:53:22 -0500

    Audio, Video file updated

---
+++ photography-website/src/components/AudioPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Here's some information about our team and our mission.</p>
+      {/* Add more content about your team, mission, and history */}
+    </div>
+  );
+};
+
+export default AboutUsPage;
