752b9e44e3704e6640aef648d0d75faf3c025c7f
makwana3011 <makwanah127@gmail.com>
2024-12-13 02:59:16 -0500

    App.js file updated

--- photography-website/src/App.js
+++ photography-website/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import GalleryPage from './components/GalleryPage';
+import AudioPage from './components/AudioPage';
+import VideoPage from './components/VideoPage';
+import AboutUsPage from './components/AboutUsPage';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/audio" element={<AudioPage />} />
+        <Route path="/video" element={<VideoPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        {/* Add more routes for contact, etc. */}
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
