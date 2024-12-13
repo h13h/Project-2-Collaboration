fa7b440f31dfa20b0d000796f0bdbef6da741668
KinjalPatel252617 <kvinseepatel@gmail.com>
2024-12-13 15:30:07 -0500

    Navbar updated

--- photography-website/src/components/Navbar.js
+++ photography-website/src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/audio">Audio</Link></li>
-        <li><Link to="/video">Video</Link></li>
-        <li><Link to="/about">About Us</Link></li>
+    <nav style={{ background: "#eee", padding: "10px" }}>
+      <ul style={{ display: "flex", listStyle: "none", gap: "20px" }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link to="/audio">Audio</Link>
+        </li>
+        <li>
+          <Link to="/video">Video</Link>
+        </li>
       </ul>
     </nav>
   );
-};
+}
 
 export default Navbar;
